4812373e63734ee60870315839bb8c31ef3e8d65
Gabriela Cruz <gabriela.cruz@concilie.com.br>
2022-07-12 19:54:07 -0300

    server + db connection

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
 import express, { Express, Request, Response } from 'express';
+// import bodyParser from 'body-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
-dotenv.config()
+dotenv.config();
 
-const app: Express = express()
-const port = process.env.PORT
+const app: Express = express();
+const port = process.env.PORT;
+app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + Typescript server!");
+const uri = `mongodb+srv://gabriela:${process.env.DB_PASS}@nodejscluster.nkvnxor.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(uri);
+
+mongoose.connection.on("connected", function () {
+  console.log("Connected to Database " + "jamstore");
 });
 
+mongoose.connection.on("error", function(err) {
+  console.log("Database error " + err);
+})
+
+require('./Routes/index')(app);
+
+app.use(cors());
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 })
