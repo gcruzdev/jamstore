1231e06bc4fb6ac18c3c00e4a088ecceea607682
Gabriela Cruz <gabrielasnts30@gmail.com>
2022-07-12 19:53:43 -0300

    user routes

---
+++ src/Routes/UserRoutes.ts
@@ -0,0 +1,10 @@
+import express, { Express } from 'express';
+const UserController = require('../Controllers/UserController');
+
+module.exports = (app: Express = express()) => {
+   app.post('/user', UserController.post);
+   app.put('/user/:id', UserController.put);
+   app.delete('/user/:id', UserController.delete);
+   app.get('/users', UserController.get);
+   app.get('/user/:id', UserController.getById);
+}
