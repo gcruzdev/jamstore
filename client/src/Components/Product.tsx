a0bec5ea37f617f569b77990c4217cb376e7a438
Gabriela Cruz <gabrielasnts30@gmail.com>
2022-07-18 20:23:44 -0300

    product display changes

--- client/src/Components/Product.tsx
+++ client/src/Components/Product.tsx
@@ -1,4 +1,6 @@
 import IProduct from "../Interfaces/IProduct" 
+import maca from '../Assets/images/maca.jpg' 
+import { Link } from "react-router-dom"
 interface ProductProps {
   ProductData: IProduct
 }
@@ -6,8 +8,10 @@ interface ProductProps {
 export default function Product({ProductData}: ProductProps) {
   return(
     <div className="product">
-      <img src="../Assets/images/maca.jpg"/>
-      <span>{ProductData.name}</span>
+      <Link to={`produto/${ProductData.code}`}>
+        <img src={maca} alt="Geléia de maçã"/>
+        <span id="product-name">{ProductData.name}</span>
+      </Link>
       <p id="product-value">R${ProductData.value}</p>
     </div>
   )
