bd1155c4fef052b6d8942e7a220d0eda4935ba27
Gabriela Cruz <gabriela.cruz@concilie.com.br>
2022-07-19 22:31:31 -0300

    changing ports

--- client/src/Components/ProductsDisplay.tsx
+++ client/src/Components/ProductsDisplay.tsx
@@ -7,7 +7,7 @@ export default function ProductsDisplay() {
   const [products, setProducts] = useState<IProduct[]>([])
 
   useEffect(() => {
-    fetch("http://localhost:8000/products")
+    fetch("http://localhost:27017/products")
       .then((res) => res.json())
       .then((data) => setProducts(data))
   })
